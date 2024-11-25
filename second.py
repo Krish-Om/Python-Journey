1936346f89c09c7846b773600668ac00502b9a70
Krishom <83640457+Krish-Om@users.noreply.github.com>
2024-11-25 21:08:35 +0545

    start of Python Journey
    Learning platform : Cisco networking (Netacad.com)

---
+++ second.py
@@ -0,0 +1,14 @@
+print("2")
+print(11_11_111)
+
+print(2 ** 3.1) #raise the power 
+print(2 // 3) #it result is rounded i.e ignores floating points
+print(2/3) #result is not rounded i.e won't ignore floating points
+
+print(12 % 4.5)
+
+#exponentiation operator uses right sided binding
+print(-3 ** 2)
+print(-2 ** 3)
+print(-(3 ** 2))
+
