44a2a7448cf3b454bf18a74115febe6b991de47d
Krishom <83640457+Krish-Om@users.noreply.github.com>
2024-11-30 16:47:28 +0545

    conditionals in python

---
+++ module3/conditionals.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter the first number:"))
+n2 = int(input("Enter the second number:"))
+
+if n1 > n2:
+    print("largest: ",n1)
+else:
+    print("largest:",n2)
---
+++ module3/spathiphyllum.py
@@ -0,0 +1,9 @@
+str = input("Enter plant:")
+str1 = "spathiphyllum"
+str2 = "Spathyphyllu"
+if str == str1:
+    print("No, I want a big Spathiphyllum")
+elif str==str2:
+    print("Yes - Spathiphyllum is best")
+else:
+    print("Spathiphyllum! Not",str)
