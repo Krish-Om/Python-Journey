0bfb3cb84e7eda7bbf4897bce76f079bd6c9df56
Krishom <83640457+Krish-Om@users.noreply.github.com>
2024-12-06 18:04:43 +0545

    some more about comprehension of list

---
+++ module3/lists/moreOnList.py
@@ -0,0 +1,10 @@
+squares = [x**2 for x in range(10)]
+
+print(squares)
+
+row = ["A" for i in range(8)]
+
+print(row)
+
+odds = [x for x in range(100) if x % 2 != 0]
+print(odds)
--- module3/lists/repeated.py
+++ module3/lists/repeated.py
@@ -1,12 +1,9 @@
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
-#
-# Write your code here.
-new_list = []
-elem_rep = my_list[0]
-occur = 0
+
+uniquList = []
+
 for i in range(len(my_list)):
-    for j=i+1 in range(len(my_list)):
-        if(arr[i] == arr[j])
-            for k=j in range()
-print("The list with unique elements only:")
-print(new_list)
+    if my_list[i] not in uniquList:
+        uniquList.append(my_list[i])
+
+print(uniquList)
