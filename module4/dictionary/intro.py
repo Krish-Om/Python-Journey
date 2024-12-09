5aa08194c4293ed6e2636010e2ea8ceac8eddabf
Krishom <83640457+Krish-Om@users.noreply.github.com>
2024-12-09 16:38:18 +0545

    dictionary and list

---
+++ module4/dictionary/intro.py
@@ -0,0 +1,14 @@
+# The dictionary is another Python data structure. It's not a sequence type and it is mutable.
+
+# Its a look-a-like of JSON
+# It stores data in a key-pair value.
+# It stores a key and a value corresponding to the key for easy accessing of the data
+
+# Example:
+dictionary = {"cat": "Hello", "dog": "china"}
+
+phone_num = {"boss": 12345345345, "Suzy": 23092398743}
+
+empty_dictionary = {}
+print(phone_num)
+print(dictionary)
