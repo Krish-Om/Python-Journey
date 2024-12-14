e41aa3b7bb79696fc8f919179da2bd0b5624f0fa
Krishom <83640457+Krish-Om@users.noreply.github.com>
2024-12-14 12:14:42 +0545

    some tiny piecies of code

--- Final-Project/Aalu_Cross/main.py
+++ Final-Project/Aalu_Cross/main.py
@@ -1,26 +1,33 @@
-#Developing a Tic-Tac-Toe TUI or Aalu_Cross using python as Final project of PE1 course from netacad.com
-board = []
+# Developing a Tic-Tac-Toe TUI or Aalu_Cross using python as Final project of PE1 course from netacad.com
+from random import randrange
 
+
+board = [[]]
+
+
+#
 def display_board(board):
     # The function accepts one parameter containing the board's current status
     # and prints it out to the console.
+    print("+-------" * 3, "+", sep="")
 
 
-def enter_move(board):
-    # The function accepts the board's current status, asks the user about their move, 
-    # checks the input, and updates the board according to the user's decision.
+# def enter_move(board):
+# The function accepts the board's current status, asks the user about their move,
+# checks the input, and updates the board according to the user's decision.
 
 
-def make_list_of_free_fields(board):
-    # The function browses the board and builds a list of all the free squares; 
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+# def make_list_of_free_fields(board):
+# The function browses the board and builds a list of all the free squares;
+# the list consists of tuples, while each tuple is a pair of row and column numbers.
 
 
-def victory_for(board, sign):
-    # The function analyzes the board's status in order to check if 
-    # the player using 'O's or 'X's has won the game
+# def victory_for(board, sign):
+# The function analyzes the board's status in order to check if
+# the player using 'O's or 'X's has won the game
 
 
-def draw_move(board):
-    # The function draws the computer's move and updates the board.
-# 
+# def draw_move(board):
+# The function draws the computer's move and updates the board.
+
+display_board(board)
