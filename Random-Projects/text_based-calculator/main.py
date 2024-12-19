083ab0ddac9da519096a27c307c7e1965d7f9ae5
Krishom <Krishombasukala@gmail.com>
2024-12-19 19:20:44 +0545

    refactor calculator input handling and error checking

--- Random-Projects/text_based-calculator/main.py
+++ Random-Projects/text_based-calculator/main.py
@@ -1,19 +1,34 @@
-try:
-    num1 = float(input("Enter a number 1:"))
-    sign = input("Sign: ")
-    num2 = float(input("Enter a number 2: "))
+def get_number(number):
+    while True:
+        
+        num = input("num "+ str(number)+ ":")
+        try:
+            return float(num)
+            break
+        except:
+            print("Invalid number,try again")
+    
+num1 = get_number(1)
+num2 = get_number(2)
 
+sign = input("Sign: ")
+valid = False
+try:
+    num1 = float(num1)
+    num2 = float(num2)
+    valid = True
+except:
+    print("Invalid Operands.")
+
+if valid:    
     if sign == "+":
         print(num1, sign, num2, " = ", num1 + num2)
     elif sign == "-":
-        print(num1, sign, num2, " = ", num1 - num2)
+            print(num1, sign, num2, " = ", num1 - num2)
     elif sign == "/":
         if num2 == 0:
             print("Division by zero")
         else:
             print(num1, sign, num2, " = ", num1 / num2)
     elif sign == "*":
-        print(num1, sign, num2, " = ", num1 * num2)
-except:
-    print("Invalid Operands.")
-
+            print(num1, sign, num2, " = ", num1 * num2)
