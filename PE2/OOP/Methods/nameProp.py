4ea4d43034e498c393e7159fea52ba0a3ae3960d
Krishom <Krishombasukala@gmail.com>
2025-01-15 10:52:00 +0545

    add nameProp.py to demonstrate class inheritance and base class retrieval in Python

--- PE2/OOP/Methods/nameProp.py
+++ PE2/OOP/Methods/nameProp.py
@@ -5,3 +5,11 @@ print(Classy.__name__)
 obj = Classy()
 
 print(type(obj).__name__)
+
+class Snake:
+    pass
+class Python(Snake):
+    pass
+
+print(Python.__name__, "is a ",Snake.__name__)
+print(Python.__bases__[0].__name__ ,"can be",Python.__name__)
