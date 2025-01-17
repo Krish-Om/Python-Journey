8bb4f0359b87a5d8c7f22852017e59c9ab0935dd
Krishom <Krishombasukala@gmail.com>
2025-01-17 21:42:03 +0545

    add Point class to represent points in a 2D plane with distance calculation methods

---
+++ PE2/OOP/Methods/pointsOnPlane.py
@@ -0,0 +1,26 @@
+import math
+class Point:
+    def __init__(self,x=0.0,y=0.0):
+        self.__x = (x)
+        self.__y =(y)
+
+
+    def getX(self):
+        return self.__x
+
+    def getY(self):
+        return self.__y
+    
+    def distance_from_xy(self,x,y):
+        return math.hypot(self.__x,self.__y)
+    
+    def distance_from_point(self,point):
+        resX = math.sqrt((self.__x**2)+(point.getX()**2))
+        resY = math.sqrt((self.__y**2)+(point.getY()**2))
+
+        return math.hypot(resX,resY)
+    
+point1 = Point(0, 0)
+point2 = Point(1, 1)
+print(point1.distance_from_point(point2))
+print(point2.distance_from_xy(2, 0))
