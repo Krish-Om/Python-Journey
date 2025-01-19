d7faafc31996b9fe85b9416675de8e3ee3967dee
Krishom <Krishombasukala@gmail.com>
2025-01-19 16:45:39 +0545

    add examples of lambda functions and their usage in Python

---
+++ PE2/MIsc/Lambdas.py
@@ -0,0 +1,12 @@
+# general syntax
+#lambda parameters : expression
+
+#some examples
+
+two = lambda: 2
+sqr = lambda x: x*x
+pwr = lambda x,y: x ** y
+
+for a in range(-2,3):
+    print(sqr(a),end=" ")
+    print(pwr(a, two()))
