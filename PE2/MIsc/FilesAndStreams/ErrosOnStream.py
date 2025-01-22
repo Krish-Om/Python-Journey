c263fc51c437d4a1daf810786e6b3836de628d2d
Krishom <Krishombasukala@gmail.com>
2025-01-22 20:38:05 +0545

    fix: enhance error handling in ErrosOnStream.py to correctly check for file existence

--- PE2/MIsc/FilesAndStreams/ErrosOnStream.py
+++ PE2/MIsc/FilesAndStreams/ErrosOnStream.py
@@ -1,11 +1,11 @@
 
 from os import strerror
-
+import errno
 try:
     s = open("/home/krishom/Workspace/python/PE2/MIsc/example.txt",'rt')
     s.close()
 except Exception as e:
-    if e.errno == errno.ENONET:
+    if (e.errno == errno.ENOENT):
         print("File don't exisits")
     elif e.errno == errno.EMFILE:
         print("You have opened too many files.")
