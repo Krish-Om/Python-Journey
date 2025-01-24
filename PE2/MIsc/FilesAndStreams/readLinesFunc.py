1605dcc4ab15e9d6e47c988a805b3ae577f40599
Krishom <Krishombasukala@gmail.com>
2025-01-24 10:16:00 +0545

    add: implement a simpler line reading function in readLinesFunc.py with improved error handling

--- PE2/MIsc/FilesAndStreams/readLinesFunc.py
+++ PE2/MIsc/FilesAndStreams/readLinesFunc.py
@@ -9,3 +9,12 @@ try:
     s.close()
 except IOError as ioe:
     print(ioe)
+
+lcnt = 0
+#simpler and concise version of above code
+try:
+    for lines in open("/home/krishom/Workspace/python/PE2/MIsc/FilesAndStreams/text.txt","rt"):
+        lcnt +=1 
+        print(lines)
+except IOError as e:
+    print(e)
