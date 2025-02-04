d4ccc6b57317e76bb09e26bb94ab884cb2d11d38
Krishom <Krishombasukala@gmail.com>
2025-02-04 20:12:12 +0545

    add: implement directory creation and current working directory retrieval in recursiveDir.py

--- PE2/misc/OSModule/recursiveDir.py
+++ PE2/misc/OSModule/recursiveDir.py
@@ -1,5 +1,12 @@
 import os
 
+os.remov("mySecondDir")
 os.makedirs("myFirstDir/mySecondDir")
 os.chdir("myFirstDir")
+print(os.listdir())
+
+
+#knowing the current working directory
+print(os.getcwd())
+
 print(os.listdir())
