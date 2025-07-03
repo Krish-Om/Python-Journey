fd579ffec7932faf188f4d10e405e2731be51a2a
Krish-Om <krishombasukala@gmail.com>
2025-07-03 21:40:37 +0545

    Implement simple reflex agent logic and add Dockerfile for environment setup

--- ML/SnakeAgents/simple_reflex_agent.py
+++ ML/SnakeAgents/simple_reflex_agent.py
@@ -1,7 +1,7 @@
-
 def simple_agent(game):
     head_x, head_y = game.snake.x[0], game.snake.y[0]
     apple_x, apple_y = game.apple.x, game.apple.y
+
     if apple_x < head_x:
         next_x, next_y = game._get_potential_head("left")
         if not game._is_potential_move_colliding(next_x, next_y):
@@ -17,13 +17,12 @@ def simple_agent(game):
         if not game._is_potential_move_colliding(next_x, next_y):
             game.snake.move_down()
             return
-    if apple_x > head_x:
+    if apple_y < head_y:
         next_x, next_y = game._get_potential_head("up")
         if not game._is_potential_move_colliding(next_x, next_y):
             game.snake.move_up()
             return
-        
-    directions = ["left", "right", "up", "down"]
-    current_dir = game.snake.direction
-    for direction in directions:
-        pass
+
+
+# There is no fallback strategy upto here
+# but we can add fallback strategy or safety strategy to score more.
